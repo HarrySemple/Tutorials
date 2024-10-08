e56576f5c9f0393e98479a4c57fed13d92cb1d8e
Harry Semple <140845080+HarrySemple@users.noreply.github.com>
2024-10-08 23:39:47 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,25 +16,34 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "Week 1",
-  "body": " Week 1  Problems For week 1, due 4pm Wednesday 9th October .   Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       All Problems  The arc length formula is given by The following identities are also useful These combine to give We also have and even     Part (c).  Use the substitution     Part (d).  Nobody knows.      Question 3   (H) Suppose a chain of length is suspended between two points and , both of which are at height . The shape of the chain is then given by with . Assume the lowest part of the hanging chain is at the point so that .   "
+  "body": " Week 1  Problems from week 1, due 4pm Wednesday 9th October .  Question 1 Find the arc length of along .   cosh along      along      along      (H)  ln along      All Problems  The arc length formula is given by The following identities are also useful These combine to give We also have and even    Part (c).  Use the substitution    Part (d).      Question 3  (H) Suppose a chain of length is suspended between two points and , both of which are at height . The shape of the chain is then given by with . Assume the lowest part of the hanging chain is at the point so that .   If the chain is hanging under its own weight alone then the shape of the chain is a catenary and for constants and . In this case, show that the constant satisfies    The total length of the chain is , so the length of the chain from the lowest point (at )to either end (at ) is . The arc length formula gives Hence      In a suspension road bridge, the hanging chains of the bridge also have to support the weight of the road, which is much heavier than the weight of the chains. In this case the shape of the cables is a simple parabola, so that Show that in this case, the constant satisfies the identity    You may use     In this case, , so As before, the arc length from the lowest point to either end is and also . Thus we have To evaluate the integral, let , so . Then      Question 4  (H) The curve with is rotated around the axis. Show that the area of the resulting surface is given by By making an appropriate substitution, show that   The surface area is given by as required. Let , so . Hence    "
 },
 {
   "id": "sec-week-1-3",
   "level": "2",
   "url": "sec-week-1.html#sec-week-1-3",
-  "type": "Problem",
+  "type": "Ex",
   "number": "1",
-  "title": "Arc Lengths.",
-  "body": " Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       All Problems  The arc length formula is given by The following identities are also useful These combine to give We also have and even     Part (c).  Use the substitution     Part (d).  Nobody knows.    "
+  "title": "Question 1.",
+  "body": "Question 1 Find the arc length of along .   cosh along      along      along      (H)  ln along      All Problems  The arc length formula is given by The following identities are also useful These combine to give We also have and even    Part (c).  Use the substitution    Part (d).     "
 },
 {
   "id": "sec-week-1-4",
   "level": "2",
   "url": "sec-week-1.html#sec-week-1-4",
-  "type": "Problem",
+  "type": "Ex",
   "number": "2",
   "title": "Question 3.",
-  "body": " Question 3   (H) Suppose a chain of length is suspended between two points and , both of which are at height . The shape of the chain is then given by with . Assume the lowest part of the hanging chain is at the point so that .  "
+  "body": "Question 3  (H) Suppose a chain of length is suspended between two points and , both of which are at height . The shape of the chain is then given by with . Assume the lowest part of the hanging chain is at the point so that .   If the chain is hanging under its own weight alone then the shape of the chain is a catenary and for constants and . In this case, show that the constant satisfies    The total length of the chain is , so the length of the chain from the lowest point (at )to either end (at ) is . The arc length formula gives Hence      In a suspension road bridge, the hanging chains of the bridge also have to support the weight of the road, which is much heavier than the weight of the chains. In this case the shape of the cables is a simple parabola, so that Show that in this case, the constant satisfies the identity    You may use     In this case, , so As before, the arc length from the lowest point to either end is and also . Thus we have To evaluate the integral, let , so . Then     "
+},
+{
+  "id": "sec-week-1-5",
+  "level": "2",
+  "url": "sec-week-1.html#sec-week-1-5",
+  "type": "Ex",
+  "number": "3",
+  "title": "Question 4.",
+  "body": "Question 4  (H) The curve with is rotated around the axis. Show that the area of the resulting surface is given by By making an appropriate substitution, show that   The surface area is given by as required. Let , so . Hence   "
 },
 {
   "id": "sec-week-2",
@@ -43,34 +52,25 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "Week 2",
-  "body": " Week 2  Text of section.   what is this?    hmmm    hmmmmm    here you go   question 1 hello   Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       The arc length formula is given by The following identities are also useful These combine to give Finally,     part (c)  Use the substitution     "
+  "body": " Week 2  Problems from week 2, due 1pm Tuesday 15th October .  Question 2 Sketch each of the following functions . Carefully explain each step of the construction. Check your answer using Desmos.              (H)           Question 4 For each of the following curves, convert the equation from rectangular to polar coordinates and sketch it.              (H)     We have Rearranging,        (H)       Taking the positive square-root and reearranging       "
 },
 {
   "id": "sec-week-2-3",
   "level": "2",
   "url": "sec-week-2.html#sec-week-2-3",
-  "type": "Question",
-  "number": "3",
-  "title": "",
-  "body": " what is this?  "
-},
-{
-  "id": "sec-week-2-7",
-  "level": "2",
-  "url": "sec-week-2.html#sec-week-2-7",
   "type": "Ex",
   "number": "4",
-  "title": "question 1.",
-  "body": "question 1 hello "
+  "title": "Question 2.",
+  "body": "Question 2 Sketch each of the following functions . Carefully explain each step of the construction. Check your answer using Desmos.              (H)          "
 },
 {
-  "id": "sec-week-2-8",
+  "id": "sec-week-2-4",
   "level": "2",
-  "url": "sec-week-2.html#sec-week-2-8",
-  "type": "Problem",
+  "url": "sec-week-2.html#sec-week-2-4",
+  "type": "Ex",
   "number": "5",
-  "title": "Arc Lengths.",
-  "body": " Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       The arc length formula is given by The following identities are also useful These combine to give Finally,     part (c)  Use the substitution    "
+  "title": "Question 4.",
+  "body": "Question 4 For each of the following curves, convert the equation from rectangular to polar coordinates and sketch it.              (H)     We have Rearranging,        (H)       Taking the positive square-root and reearranging      "
 },
 {
   "id": "sec-arc-length-derivation",
@@ -79,7 +79,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "Arc Length",
-  "body": " Arc Length  Text of section.  "
+  "body": " Arc Length  For small sections of a curve we have that Rearranging, this gives Sending gives us Finally, we integrate to obtain   "
 },
 {
   "id": "sec-normal-dist-derivation",
@@ -88,7 +88,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "The Normal Distribution I",
-  "body": " The Normal Distribution I  Imagine you are throwing darts onto the -plane, aiming for the origin . After hundereds of darts, what does the plane look like?  Assuming you're not a perfect shot, we would expect some variation - a dense cluster at the centre that thins out as we look further away from the origin. We also expect is this scattering to be radially symmetric.   Let be a small region in the plane. We can model the probability on a dart landing in as   where is the area of and is some monotone decreasing funtion , with the distance away from the origin of a token point . Note that doesn't appear in our model due to radial symmetry.   Is it measurable?   is known as the 2D Lebesgue measure. Precisely defining this function has to be done with extreme care in order to avoid paradoxes caused by certain difficult (non-measurable) subsets of . More detail can be found here .   For a single dart, the and coordinates are independent, allowing us to introduce   where is another monotone decreasing function, with the relation   Setting , we have  where is some constant. Thus,   dividing through by , we have  Define such that . We have that satisfies Do we know what type of function is likely to be? Can you think of an example?     Find a function that satisfies What is the general form of functions that satisfy ?      Find a function that satisfies What is the general form of functions that satisfy ?      "
+  "body": " The Normal Distribution I  Imagine you are throwing darts onto the -plane, aiming for the origin . After hundereds of darts, what does the plane look like?  Assuming you're not a perfect shot, we would expect some variation - a dense cluster at the centre that thins out further away from the origin. We also expect is the scattering to be radially symmetric.   Let be a small region in the plane. We can model the probability on a dart landing in as   where is the area of and is some monotone decreasing funtion , with the distance away from the origin of a token point . Note that doesn't appear in our model due to radial symmetry.   Is it measurable?   is known as the 2D Lebesgue measure. Precisely defining this function has to be done with extreme care in order to avoid paradoxes caused by certain difficult (non-measurable) subsets of . More detail can be found here .   For a single dart, the and coordinates are independent, allowing us to introduce   where is another monotone decreasing function, with the relation   Setting , we have  where is some constant. Thus,   dividing through by , we have  Define such that . We have that satisfies Do we know what type of function is likely to be? Can you think of an example?     Find a function that satisfies What is the general form of functions that satisfy ?      Find a function that satisfies What is the general form of functions that satisfy ?  Stay with exponentials!       "
 },
 {
   "id": "sec-normal-dist-derivation-3",
@@ -124,7 +124,7 @@ var ptx_lunr_docs = [
   "type": "Ex",
   "number": "7",
   "title": "",
-  "body": "Find a function that satisfies What is the general form of functions that satisfy ?     "
+  "body": "Find a function that satisfies What is the general form of functions that satisfy ?  Stay with exponentials!      "
 },
 {
   "id": "sec-normal-dist-derivation-2",
@@ -133,7 +133,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "The Normal Distribution II",
-  "body": " The Normal Distribution II   Assumptions So Far     Distribution is radially symmetric.      Probability of landing in a small region is proportional to that region.      Probability of landing in a region monotonically decreases as the distance away from the origin increases.      From , we have that where A is some constant. To ensure is a monotone decreasing function, we set   We now have a candidate probability density function (pdf)  All that remains of our derivation is specifying the variables and . This is done by setting   "
+  "body": " The Normal Distribution II   Assumptions So Far     Distribution is radially symmetric.      Probability of landing in a small region is proportional to the size of that region.      Probability of landing in a region monotonically decreases as the distance away from the origin increases.      From , we have that where A is some constant. To ensure is monotone decreasing, set   We now have a candidate probability density function (PDF) for the coordinate That is, the probability of the coordinate of the dart lying in an interval is given by All that remains of our derivation is specifying the variables and . This is done by setting substituting , the problem becomes This is where we come across the Gaussian Integral , an intergral imposibble to compute using standard techniques. Luckily for us, multivariable calculus provides all the tools we require.   The Gaussian Integral  Set . Then Since the integral is constant, we have Since is constant with respect to , we have Simplifying the integrand leaves us with Now we want to make a change of variables into polar coordinates. To do this, we must apply a scaling to the integrand (much as we would do for one-dimensional integration). For integration in higher dimensions, this manifests as the Jacobian . substituting in , we have By choosing appropriate bounds, we now complete the change of variables This leaves us with the satisfying result    "
 },
 {
   "id": "sec-normal-dist-derivation-2-4",
@@ -142,7 +142,34 @@ var ptx_lunr_docs = [
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
-  "body": "probability density function (pdf) "
+  "body": "probability density function (PDF) Gaussian Integral "
+},
+{
+  "id": "subsec-gauss-2",
+  "level": "2",
+  "url": "sec-normal-dist-derivation-2.html#subsec-gauss-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "change of variables Jacobian "
+},
+{
+  "id": "sec-normal-dist-derivation-3",
+  "level": "1",
+  "url": "sec-normal-dist-derivation-3.html",
+  "type": "Section",
+  "number": "",
+  "title": "The Normal Distribution III",
+  "body": " The Normal Distribution III   Specifying Variables  Combining and , we have Thus, our PDF can be simplifiied as   Our new aim is to write the variance in terms of the PDF, defined as where is the mean, which is in our darts paradigm. Thus This gives the relation Thus, Lastly, we take into account cases where we have a non-zero mean. This refers to a translation along the axis, giving us a final PDF of  See it for yourself!      "
+},
+{
+  "id": "subsec-spec-var-3",
+  "level": "2",
+  "url": "sec-normal-dist-derivation-3.html#subsec-spec-var-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "variance "
 },
 {
   "id": "backmatter-2",
