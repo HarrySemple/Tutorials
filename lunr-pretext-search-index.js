1ffe73d6d6d15e61dc974de2847abb9a00595faf
Harry Semple <140845080+HarrySemple@users.noreply.github.com>
2024-10-08 19:05:03 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -14,25 +14,25 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-week-1.html",
   "type": "Section",
-  "number": "1.1",
+  "number": "",
   "title": "Week 1",
-  "body": " Week 1  Problems For week 1, due 4pm Wednesday 9th October .   Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       The arc length formula is given by The following identities are also useful These combine to give We also have and even     Part (c).  Use the substitution     Part (d).  Nobody knows.      Question 3   (H) Suppose a chain of length is suspended between two points and , both of which are at height . The shape of the chain is then given by with . Assume the lowest part of the hanging chain is at the point so that .   "
+  "body": " Week 1  Problems For week 1, due 4pm Wednesday 9th October .   Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       All Problems  The arc length formula is given by The following identities are also useful These combine to give We also have and even     Part (c).  Use the substitution     Part (d).  Nobody knows.      Question 3   (H) Suppose a chain of length is suspended between two points and , both of which are at height . The shape of the chain is then given by with . Assume the lowest part of the hanging chain is at the point so that .   "
 },
 {
   "id": "sec-week-1-3",
   "level": "2",
   "url": "sec-week-1.html#sec-week-1-3",
   "type": "Problem",
-  "number": "1.1.1",
+  "number": "1",
   "title": "Arc Lengths.",
-  "body": " Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       The arc length formula is given by The following identities are also useful These combine to give We also have and even     Part (c).  Use the substitution     Part (d).  Nobody knows.    "
+  "body": " Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       All Problems  The arc length formula is given by The following identities are also useful These combine to give We also have and even     Part (c).  Use the substitution     Part (d).  Nobody knows.    "
 },
 {
   "id": "sec-week-1-4",
   "level": "2",
   "url": "sec-week-1.html#sec-week-1-4",
   "type": "Problem",
-  "number": "1.1.2",
+  "number": "2",
   "title": "Question 3.",
   "body": " Question 3   (H) Suppose a chain of length is suspended between two points and , both of which are at height . The shape of the chain is then given by with . Assume the lowest part of the hanging chain is at the point so that .  "
 },
@@ -41,7 +41,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-week-2.html",
   "type": "Section",
-  "number": "1.2",
+  "number": "",
   "title": "Week 2",
   "body": " Week 2  Text of section.   what is this?    hmmm    hmmmmm    here you go   question 1 hello   Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       The arc length formula is given by The following identities are also useful These combine to give Finally,     part (c)  Use the substitution     "
 },
@@ -50,7 +50,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-week-2.html#sec-week-2-3",
   "type": "Question",
-  "number": "1.2.1",
+  "number": "3",
   "title": "",
   "body": " what is this?  "
 },
@@ -58,8 +58,8 @@ var ptx_lunr_docs = [
   "id": "sec-week-2-7",
   "level": "2",
   "url": "sec-week-2.html#sec-week-2-7",
-  "type": "Checkpoint",
-  "number": "1.2.2",
+  "type": "Ex",
+  "number": "4",
   "title": "question 1.",
   "body": "question 1 hello "
 },
@@ -68,7 +68,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-week-2.html#sec-week-2-8",
   "type": "Problem",
-  "number": "1.2.3",
+  "number": "5",
   "title": "Arc Lengths.",
   "body": " Arc Lengths  Find the arc length of along .   cosh along      along      along      (H)  ln along       The arc length formula is given by The following identities are also useful These combine to give Finally,     part (c)  Use the substitution    "
 },
@@ -77,7 +77,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-arc-length-derivation.html",
   "type": "Section",
-  "number": "2.1",
+  "number": "",
   "title": "Arc Length",
   "body": " Arc Length  Text of section.  "
 },
@@ -86,9 +86,63 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-normal-dist-derivation.html",
   "type": "Section",
-  "number": "2.2",
-  "title": "The Normal Distribution",
-  "body": " The Normal Distribution  Text of section.  "
+  "number": "",
+  "title": "The Normal Distribution I",
+  "body": " The Normal Distribution I  Imagine you are throwing darts onto the -plane, aiming for the origin . After hundereds of darts, what does the plane look like?  Assuming you're not a perfect shot, we would expect some variation - a dense cluster at the centre that thins out as we look further away from the origin. We also expect is this scattering to be radially symmetric.   Let be a small region in the plane. We can model the probability on a dart landing in as   where is the area of and is some monotone decreasing funtion , with the distance away from the origin of a token point . Note that doesn't appear in our model due to radial symmetry.   Is it measurable?   is known as the 2D Lebesgue measure. Precisely defining this function has to be done with extreme care in order to avoid paradoxes caused by certain difficult (non-measurable) subsets of . More detail can be found here .   For a single dart, the and coordinates are independent, allowing us to introduce   where is another monotone decreasing function, with the relation   Setting , we have  where is some constant. Thus,   dividing through by , we have  Define such that . We have that satisfies Do we know what type of function is likely to be? Can you think of an example?     Find a function that satisfies What is the general form of functions that satisfy ?      Find a function that satisfies What is the general form of functions that satisfy ?      "
+},
+{
+  "id": "sec-normal-dist-derivation-3",
+  "level": "2",
+  "url": "sec-normal-dist-derivation.html#sec-normal-dist-derivation-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "radially symmetric. "
+},
+{
+  "id": "sec-normal-dist-derivation-6",
+  "level": "2",
+  "url": "sec-normal-dist-derivation.html#sec-normal-dist-derivation-6",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "monotone decreasing "
+},
+{
+  "id": "sec-normal-dist-derivation-20",
+  "level": "2",
+  "url": "sec-normal-dist-derivation.html#sec-normal-dist-derivation-20",
+  "type": "Ex",
+  "number": "6",
+  "title": "",
+  "body": "Find a function that satisfies What is the general form of functions that satisfy ?     "
+},
+{
+  "id": "ex-norm-1",
+  "level": "2",
+  "url": "sec-normal-dist-derivation.html#ex-norm-1",
+  "type": "Ex",
+  "number": "7",
+  "title": "",
+  "body": "Find a function that satisfies What is the general form of functions that satisfy ?     "
+},
+{
+  "id": "sec-normal-dist-derivation-2",
+  "level": "1",
+  "url": "sec-normal-dist-derivation-2.html",
+  "type": "Section",
+  "number": "",
+  "title": "The Normal Distribution II",
+  "body": " The Normal Distribution II   Assumptions So Far     Distribution is radially symmetric.      Probability of landing in a small region is proportional to that region.      Probability of landing in a region monotonically decreases as the distance away from the origin increases.      From , we have that where A is some constant. To ensure is a monotone decreasing function, we set   We now have a candidate probability density function (pdf)  All that remains of our derivation is specifying the variables and . This is done by setting   "
+},
+{
+  "id": "sec-normal-dist-derivation-2-4",
+  "level": "2",
+  "url": "sec-normal-dist-derivation-2.html#sec-normal-dist-derivation-2-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "probability density function (pdf) "
 },
 {
   "id": "backmatter-2",
